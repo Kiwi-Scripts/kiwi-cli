f5459c7f3019ae48940e254fc4f22940c28ae488
Miya <118014697+kyaaMiya@users.noreply.github.com>
2026-03-24 12:40:47 +0100

    chore: add debug logging to module loader

--- src/lib/util/module.loader.ts
+++ src/lib/util/module.loader.ts
@@ -47,7 +47,7 @@ export async function loadModule(filePath: string, options: LoadModuleOptions = 
     try {
       return assertDefaultExport(await loadTypeScriptModule(filePath, options.silent), filePath, options);
     } catch (error) {
-      if (options.silent) return null;
+      if (options.silent) return terminateSilent(`Failed to load TypeScript module: '${filePath}'. Error: ${(error as Error).message}`);
       logger.ml.warn( 'Tried to load a typescript module and failed.',
         'Loading .ts files requires the optional dependency "tsx".',
         `Install it with: ${chalk.reset('npm install tsx')}`,
@@ -56,11 +56,7 @@ export async function loadModule(filePath: string, options: LoadModuleOptions = 
     }
   }
 
-  if (options.silent) return null;
-
-  const basename = path.basename(filePath);
-  logger.warn(`Unsupported file extension '${ext}' for module '${basename}'.`);
-  throw new Error(`Unsupported module format: ${basename}`);
+  return terminate(`Unsupported module format: ${path.basename(filePath)}`, options.silent);
 }
 
 // === json loading =================================================
@@ -77,11 +73,10 @@ export function loadJsonModule(filePath: string) {
 export async function loadJavaScriptModule(filePath: string, silent = false) {
   const ext = path.extname(filePath);
   if (!JS_EXTENSIONS.includes(ext)) {
-    if (silent) return null;
-    throw new Error(`Invalid file type: ${filePath}. Only JavaScript files are supported.`);
+    return terminate(`Invalid file type: '${filePath}'. Only JavaScript files are supported.`, silent);
   }
   const abs = assertFileExists(filePath);
-  logger.debug('Loading JavaScript config file:', abs);
+  logger.debug('Loading JavaScript file:', abs);
   const mod = await import(url.pathToFileURL(abs).href);
   return (mod.default ?? mod) as KiwiConfig;
 }
@@ -91,12 +86,11 @@ export async function loadJavaScriptModule(filePath: string, silent = false) {
 export async function loadTypeScriptModule(filePath: string, silent = false) {
   const ext = path.extname(filePath);
   if (!TS_EXTENSIONS.includes(ext)) {
-    if (silent) return null;
-    throw new Error(`Invalid file type: ${filePath}. Only TypeScript files are supported.`);
+    return terminate(`Invalid file type: '${filePath}'. Only TypeScript files are supported.`, silent);
   }
 
   const abs = assertFileExists(filePath);
-  logger.debug('Loading TypeScript config file:', abs);
+  logger.debug('Loading TypeScript file:', abs);
   return await importWithTsx(abs);
 }
 
@@ -114,7 +108,7 @@ async function importWithTsx(filePath: string) {
 function assertFileExists(filePath: string) {
   const abs = path.resolve(filePath);
   if (!fs.existsSync(abs)) {
-    throw new Error(`File not found: ${abs}`);
+    throw new Error(`File not found: '${abs}'`);
   }
   return abs;
 }
@@ -122,24 +116,31 @@ function assertFileExists(filePath: string) {
 function isAllowedExtension(ext: string, allowedExtensions: string[] | undefined, silent = false) {
   if (!allowedExtensions) return true;
   if (allowedExtensions.includes(ext)) return true;
-  if (silent) return false;
-  throw new Error(`File extension ${ext} is not allowed for this module type.`);
+  return terminate(`File extension '${ext}' is not allowed for this module type.`, silent);
 }
 
 function isAllowedTypeSuffix(filePath: string, typeSuffix: string | undefined, silent = false) {
   if (!typeSuffix) return true;
   const suffix = path.basename(filePath, path.extname(filePath)).split('.').pop();
   if (suffix === typeSuffix) return true;
-  if (silent) return false;
-  const basename = path.basename(filePath);
-  throw new Error(`File ${basename} does not match the required type suffix '${typeSuffix}'.`);
+
+  return terminate(`File '${path.basename(filePath)}' does not match the required type suffix '${typeSuffix}'.`, silent);
 }
 
 function assertDefaultExport(mod: any, filePath: string, {required, silent}: {required?: boolean, silent?: boolean}) {
   if (!mod || (required && !mod.default)) {
-    if (silent) return null;
-    const basename = path.basename(filePath);
-    throw new Error(`File ${basename} does not have a default export.`);
+    return terminate(`Module '${path.basename(filePath)}' does not have a default export.`, silent);
   }
+  logger.debug(`Module '${path.basename(filePath)}' has a default export.`);
   return mod;
+}
+
+function terminate(message: string, silent = false) {
+  if (silent) return terminateSilent(message);
+  throw new Error(message);
+}
+
+function terminateSilent(message: string) {
+  logger.debug(message);
+  return null;
 }
