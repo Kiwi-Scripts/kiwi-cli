617aa5eaf81cb5cba7ae3772a1a01edb638fd1d0
Miya <118014697+kyaaMiya@users.noreply.github.com>
2026-03-26 14:56:32 +0100

    bug: fix tsup dev config

--- tsup.dev.config.ts
+++ tsup.dev.config.ts
@@ -4,36 +4,36 @@ import pkg from "./package.json";
 export default defineConfig([
   {
     entry: {
-      index: "src/index.ts"
-    },
-    format: ["esm", "cjs"],
-    dts: true,
-    sourcemap: true,
-    clean: true,
-    target: "node18",
-    platform: "node",
-    external: ['tsx'],
-    define: {
-      __VERSION__: JSON.stringify(pkg.version)
-    }
-  },{
-    entry: {
       templates: "src/lib/templates/template.registry.ts",
       cli: "src/cli.ts",
     },
     format: ["esm"],
     dts: true,
     sourcemap: true,
-    clean: false,
+    clean: true,
     minify: false,
     target: "node18",
     platform: "node",
     splitting: false,
     external: [
-      '@lib/templates/template.registry',
+      'templates',
       'tsx',
       'clipboardy'
     ],
+    define: {
+      __VERSION__: JSON.stringify(pkg.version)
+    }
+  },{
+    entry: {
+      index: "src/index.ts"
+    },
+    format: ["esm", "cjs"],
+    dts: true,
+    sourcemap: true,
+    clean: false,
+    target: "node18",
+    platform: "node",
+    external: ['./cli'],
     define: {
       __VERSION__: JSON.stringify(pkg.version)
     }
